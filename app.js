8b92d2037c0325e18db50c3ade32057875264fce
Junia Josephine <junia.josephine@gmail.com>
2021-09-20 18:22:58 +0530

    Updated alerts and prompts in JS

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-var user = prompt("Enter your name");
-alert("Welcome "+ user+" to try our Minion app");
+//input
+    var user = prompt("Enter your name");
+
+//processing 
+    var welcomeMessage = "Welcome "+ user+" to try our Minion app";
+
+//output
+    alert(welcomeMessage);
+
