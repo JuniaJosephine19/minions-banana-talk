4edb540c2804d4a38e8ef972a4a90587acbbf732
Junia Josephine <junia.josephine@gmail.com>
2021-09-21 01:05:53 +0530

    Added app.js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-//input
-    var user = prompt("Enter your name");
+var btnTranslator= document.querySelector("#btn-trans");
+var inputText= document.querySelector("#in-txt-area");
+var outputText= document.querySelector("#out-txt-area");
 
-//processing 
-    var welcomeMessage = "Welcome "+ user+" to try our Minion app";
+var url = "https://api.funtranslations.com/translate/minion";
 
-//output
-    alert(welcomeMessage);
+function constructUrl(text){
+    return url + "?text=" + text;
+}
 
+function errorHandler(error){
+    console.log("Error occured! " + error);
+    alert("Error occured! Something went wrong!");
+}
+
+function getTranslation(input){
+    var inputTxt = input.value;
+    fetch(constructUrl(inputTxt))
+    .then(Response => Response.json())
+    .then(json =>{
+        var translatedTxt = json.contents.translated;
+        outputText.innerText = translatedTxt;
+    })
+    .catch(errorHandler);
+}
+
+btnTranslator.addEventListener("click", function clickEventHandler(){
+    getTranslation(inputText);
+})
