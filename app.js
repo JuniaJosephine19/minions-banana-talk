85509b900034b66054bce8ece8d3059b87f78554
Junia Josephine <junia.josephine@gmail.com>
2021-09-21 23:32:13 +0530

    refactor: updated textarea placeholder and Beautify

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-var btnTranslator= document.querySelector("#btn-trans");
-var inputText= document.querySelector("#in-txt-area");
-var outputText= document.querySelector("#out-txt-area");
+var btnTranslator = document.querySelector("#btn-trans");
+var inputText = document.querySelector("#in-txt-area");
+var outputText = document.querySelector("#out-txt-area");
 
 var url = "https://api.funtranslations.com/translate/minion";
 
-function constructUrl(text){
+function constructUrl(text) {
     return url + "?text=" + text;
 }
 
-function errorHandler(error){
+function errorHandler(error) {
     console.log("Error occured! " + error);
     alert("Error occured! Something went wrong!");
 }
 
-function getTranslation(input){
+function getTranslation(input) {
     var inputTxt = input.value;
     fetch(constructUrl(inputTxt))
-    .then(Response => Response.json())
-    .then(json =>{
-        var translatedTxt = json.contents.translated;
-        outputText.innerText = translatedTxt;
-    })
-    .catch(errorHandler);
+        .then(Response => Response.json())
+        .then(json => {
+            var translatedTxt = json.contents.translated;
+            outputText.innerText = translatedTxt;
+        })
+        .catch(errorHandler);
 }
 
-btnTranslator.addEventListener("click", function clickEventHandler(){
+btnTranslator.addEventListener("click", function clickEventHandler() {
     getTranslation(inputText);
 })
